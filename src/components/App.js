d2867c2fe3d19d95206c4e42684dc55b7201f9c0
Brian Bycynski <high5bri@hotmail.com>
2017-01-11 15:59:13 -0500

    Form over Fun

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,16 @@ class App extends Component {
 
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <ul>
+        <li>Hello World</li>
+        <li>Hello World Again</li>
+        <li>Repeat</li>
+        <li>Again</li>
+        <li>And Again</li>
+      </ul>
+      <h1>I am here for no reason</h1>
+      <button>Button to nowhere</button>
+      <a href='https://www.theironyard.com/'>Link to the Iron Yard</a>
     </div>
   }
 }
